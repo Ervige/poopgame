f74780dabc21c9614e189664a33767b806977f97
Lukasz Rdzanek <lukasz.rdzanek@pm.me>
2025-01-04 01:32:37 +0100

    feat: fix move and spawn dog scripts

--- js/dog/moveDog.js
+++ js/dog/moveDog.js
@@ -1,36 +1,53 @@
-import { BOARD_SIZE } from "../../game_config.js";
-import { drawNumbers } from "../utils/drawNumbers.js";
+import { BOARD_SIZE, DIRECTION } from "../../game_config.js";
+
+import { calculatePosition } from "../utils/calculatePosition.js";
+import { checkBoundaryConditions } from "../utils/checkBoundaryConditions.js";
+import { switchDirectionToOpposite } from "../utils/switchDirectionToOpposite.js";
 
 export function moveDog() {
   const dog = document.querySelector(".dog");
+
   if (!dog) {
-    console.error("Dog element not found.");
+    console.error("Dog element is not found.");
     return;
   }
 
-  let currentDogPosition = parseInt(dog.getAttribute("data-position"));
+  const directions = Object.keys(DIRECTION);
 
-  const isValidPosition = (position) => position >= 1 && position <= BOARD_SIZE;
+  const player = document.querySelector(".player");
 
-  const directions = [-1, 1, -Math.sqrt(BOARD_SIZE), Math.sqrt(BOARD_SIZE)];
-  let newDogPosition;
+  const currentDogPosition = +dog.getAttribute("data-position");
 
-  do {
-    const randomDirection = directions[Math.floor(Math.random() * directions.length)];
-    newDogPosition = currentDogPosition + randomDirection;
-  } while (!isValidPosition(newDogPosition));
+  let randomDirection =
+    directions[Math.floor(Math.random() * directions.length)];
+
+  let newDogPosition = calculatePosition(currentDogPosition, randomDirection);
+
+  const currentPlayerPosition = +player.getAttribute("data-position");
+
+  if (
+    !checkBoundaryConditions(newDogPosition) ||
+    !newDogPosition ||
+    newDogPosition < 0 ||
+    newDogPosition > BOARD_SIZE
+  ) {
+    newDogPosition = calculatePosition(
+      currentDogPosition,
+      switchDirectionToOpposite(randomDirection)
+    );
+  }
+
+  if (newDogPosition === currentPlayerPosition) {
+    return;
+  }
 
   dog.setAttribute("data-position", newDogPosition);
 
   const newField = document.querySelector(`[data-fieldid="${newDogPosition}"]`);
-  if (newField) {
+
+  if (newField && newDogPosition) {
     newField.appendChild(dog);
-    console.log(`Dog moved to position: ${newDogPosition}`);
   } else {
     console.error(`Field with data-fieldid="${newDogPosition}" not found.`);
   }
-}
-
-export function startDogMovement() {
-  setInterval(moveDog, 1000);
 }
